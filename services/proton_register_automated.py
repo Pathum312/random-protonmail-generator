f428321408e9b380c947fa0705496660fc6aa960
Pathum312 <pathum.2019557@iit.ac.lk>
2024-12-15 02:46:08 +0530

    Fixed the issue causing the initial form not to be filled and fixed submit button obscurity issue

--- services/proton_register_automated.py
+++ services/proton_register_automated.py
@@ -1,8 +1,10 @@
-from .web_scraper import Web_Scraper
-from .account_details import Account_Details
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
+
+from .account_details import Account_Details
+from .web_scraper import Web_Scraper
+
 
 class Registration_Process:
     def __init__(self, driver: WebDriver) -> None:
@@ -17,107 +19,84 @@ class Registration_Process:
         # To set the email value, certain steps are needed:
         # First, find the iframe
         username_iframe: WebElement = self.web_scraper.getElement(
-            timeout=10,
-            by=By.CSS_SELECTOR,
-            value='iframe[title="Username"]'
+            timeout=10, by=By.CSS_SELECTOR, value='iframe[title="Email address"]'
         )
-    
+
         # Second, switch the driver focus to that iframe
         self.driver.switch_to.frame(frame_reference=username_iframe)
-    
+
         # Third, find and set the value for the email as with the other input elements
         email: WebElement = self.web_scraper.getElement(
-            timeout=10, 
-            by=By.ID, 
-            value='email'
+            timeout=10, by=By.ID, value="email"
         )
         email.send_keys(self.username)
-        
+
         # Finally, switch back to the default document
         self.driver.switch_to.default_content()
-        
+
         # Set a password value
         password: WebElement = self.web_scraper.getElement(
-            timeout=10, 
-            by=By.ID, 
-            value='password'
+            timeout=10, by=By.ID, value="password"
         )
         password.send_keys(self.password)
-    
+
         # Set a repeat-password value
-        repeat_password: WebElement = self.web_scraper.getElement( 
-            timeout=10, 
-            by=By.ID, 
-            value='repeat-password'
+        repeat_password: WebElement = self.web_scraper.getElement(
+            timeout=10, by=By.ID, value="repeat-password"
         )
         repeat_password.send_keys(self.password)
-        
+
         # Click the sumbit button
-        submit_button: WebElement = self.web_scraper.getElement(
-            timeout=10,
-            by=By.CSS_SELECTOR,
-            value='button[type="submit"]'
+        submit_button: WebElement = self.web_scraper.getClickableElement(
+            timeout=10, by=By.CSS_SELECTOR, value='button[type="submit"]'
         )
         submit_button.click()
 
     def select_free_plan(self) -> None:
         # Click the Continue with Free button
         free_plan_button: WebElement = self.web_scraper.getElement(
-            timeout=10,
-            by=By.XPATH,
-            value="//button[text()='Continue with Free']"
+            timeout=10, by=By.XPATH, value="//button[text()='Continue with Free']"
         )
         free_plan_button.click()
-    
+
         # Click the No, thanks button
         no_thanks_button: WebElement = self.web_scraper.getElement(
-            timeout=10,
-            by=By.XPATH,
-            value="//button[text()='No, thanks']"
+            timeout=10, by=By.XPATH, value="//button[text()='No, thanks']"
         )
         no_thanks_button.click()
 
     def set_display_name(self) -> None:
         # Set a display name value
         display_name: WebElement = self.web_scraper.getElement(
-            timeout=30, 
-            by=By.ID, 
-            value='displayName'
+            timeout=30, by=By.ID, value="displayName"
         )
         # The display name is set by default to the username, so clear it
         display_name.clear()
         # Then set the full name as the display name
         display_name.send_keys(self.full_name)
-        
+
         # Click the Continue button
         continue_button: WebElement = self.web_scraper.getElement(
-            timeout=30,
-            by=By.XPATH,
-            value="//button[text()='Continue']"
+            timeout=30, by=By.XPATH, value="//button[text()='Continue']"
         )
         continue_button.click()
 
     def skip_recovery_method(self) -> None:
         # Click the Maybe later button
         maybe_later_button: WebElement = self.web_scraper.getElement(
-            timeout=50,
-            by=By.XPATH,
-            value="//button[text()='Maybe later']"
+            timeout=50, by=By.XPATH, value="//button[text()='Maybe later']"
         )
         maybe_later_button.click()
-        
+
         # Click the Confirm button
         confirm_button: WebElement = self.web_scraper.getElement(
-            timeout=50,
-            by=By.XPATH,
-            value="//button[text()='Confirm']"
+            timeout=50, by=By.XPATH, value="//button[text()='Confirm']"
         )
         confirm_button.click()
 
     def get_account_details(self) -> dict[str, str]:
         return {
-            'name': self.full_name,
-            'username': self.username,
-            'password': self.password
+            "name": self.full_name,
+            "username": self.username,
+            "password": self.password,
         }
-        
--- services/web_scraper.py
+++ services/web_scraper.py
@@ -1,8 +1,9 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 class Web_Scraper:
     def __init__(self, driver: WebDriver) -> None:
@@ -11,28 +12,30 @@ class Web_Scraper:
     def elementDetails(self, element: WebElement) -> None:
         # Finding the element's attributes and properties
         element_tag_name: str = element.tag_name
-        element_id: str | None = element.get_attribute(name='id') # type: ignore
-        element_for: str | None = element.get_attribute(name='for') # type: ignore
-        element_class: str | None = element.get_attribute(name='class') # type: ignore
-        element_value: str | None = element.get_attribute(name='value') # type: ignore
+        element_id: str | None = element.get_attribute(name="id")  # type: ignore
+        element_for: str | None = element.get_attribute(name="for")  # type: ignore
+        element_class: str | None = element.get_attribute(name="class")  # type: ignore
+        element_value: str | None = element.get_attribute(name="value")  # type: ignore
         element_text: str = element.text
-    
-        print('')
-        print(f'Tag Name: <{element_tag_name}>')
-        print(f'ID: {element_id}')
-        print(f'For: {element_for}')
-        print(f'Class: {element_class}')
-        print(f'Value: {element_value}')
-        print(f'Text: {element_text}')
-        print('')
+
+        print("")
+        print(f"Tag Name: <{element_tag_name}>")
+        print(f"ID: {element_id}")
+        print(f"For: {element_for}")
+        print(f"Class: {element_class}")
+        print(f"Value: {element_value}")
+        print(f"Text: {element_text}")
+        print("")
 
     def showElementDetails(self, timeout: float, value: str) -> None:
         # Finding the requested elements
-        elements: list[WebElement] = WebDriverWait(driver=self.driver, timeout=timeout).until(
+        elements: list[WebElement] = WebDriverWait(
+            driver=self.driver, timeout=timeout
+        ).until(
             method=EC.presence_of_all_elements_located(locator=(By.TAG_NAME, value))
         )
-    
-        for element in  elements:
+
+        for element in elements:
             self.elementDetails(element=element)
 
     def getElement(self, timeout: float, by: str, value: str) -> WebElement:
@@ -40,5 +43,13 @@ class Web_Scraper:
         element: WebElement = WebDriverWait(driver=self.driver, timeout=timeout).until(
             method=EC.visibility_of_element_located(locator=(by, value))
         )
-    
+
+        return element
+
+    def getClickableElement(self, timeout: float, by: str, value: str) -> WebElement:
+        # Finding a visible element in the document
+        element: WebElement = WebDriverWait(driver=self.driver, timeout=timeout).until(
+            method=EC.element_to_be_clickable(mark=(by, value))
+        )
+
         return element
